1ee094683fdffde6b956d32e51a63ba749f1da4d
Rupsi Kumari <66114602+Rupsi-Singh@users.noreply.github.com>
2021-01-09 11:40:04 +0530

    Update RemoveDuplicateTest.java

--- lab2/src/test/java/com/cg/lab2/util/RemoveDuplicateTest.java
+++ lab2/src/test/java/com/cg/lab2/util/RemoveDuplicateTest.java
@@ -1,3 +1,9 @@
+/**
+*Test cases for a program  which accepts an integer array and removes all the duplicates in the array. Return the resulting array in descending order
+*@author- Rupsi
+*/
+
+
 package com.cg.lab2.util;
 
 import static org.junit.Assert.*;
